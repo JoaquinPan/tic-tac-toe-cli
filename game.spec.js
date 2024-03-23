ca3cbfdfae261d1a9858a5b5692c95dcef311cd1
JoaquinPan <boson159456@gmail.com>
2024-03-23 23:06:11 +0800

    Set up jest

---
+++ game.spec.js
@@ -0,0 +1,13 @@
+const { sum } = require("./game");
+
+describe("sum", () => {
+  test("1+1", () => {
+    expect(sum(1, 2)).toBe(3);
+  });
+  test("2+2", () => {
+    expect(sum(2, 2)).toBe(4);
+  });
+  test("-100+3", () => {
+    expect(sum(-100, 3)).toBe(-97);
+  });
+});
