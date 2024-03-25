5ed4ece6859eb830e07b77b40dd8a0c5b9553373
JoaquinPan <boson159456@gmail.com>
2024-03-26 00:00:28 +0800

    Add core game logic

--- game.js
+++ game.js
@@ -1,7 +1,115 @@
-function sum(a, b) {
-  return a + b;
+const P1 = "X";
+const P2 = "O";
+const EMPTY = " ";
+
+function createEmptyBoard() {
+  return [
+    [EMPTY, EMPTY, EMPTY],
+    [EMPTY, EMPTY, EMPTY],
+    [EMPTY, EMPTY, EMPTY],
+  ];
+}
+
+function updateGame(player, pos, state) {
+  const i = pos[0];
+  const j = pos[1];
+
+  if (i < 0 || i >= state.length) {
+    return [state, false];
+  }
+  if (j < 0 || j >= state[i].length) {
+    return [state, false];
+  }
+  if (state[i][j] !== EMPTY) {
+    return [state, false];
+  }
+
+  const newState = state.map((row, rowIndex) => {
+    if (rowIndex === i) {
+      return row.map((cell, colIndex) => {
+        if (colIndex === j) {
+          return player;
+        }
+        return cell;
+      });
+    }
+    return row;
+  });
+
+  return [newState, true];
+}
+
+function checkWinner(state) {
+  const winner = checkRows(state) || checkCols(state) || checkDiags(state);
+  if (winner !== null) {
+    return winner;
+  }
+
+  if (isBoardFull(state)) {
+    return "draw";
+  }
+
+  return null;
+}
+
+function checkRows(state) {
+  for (let i = 0; i < state.length; i++) {
+    const row = state[i];
+    const winner = checkRow(row);
+    if (winner !== null) {
+      return winner;
+    }
+  }
+  return null;
+}
+
+function checkRow(row) {
+  const first = row[0];
+  if (first === EMPTY) {
+    return null;
+  }
+  for (let i = 1; i < row.length; i++) {
+    if (row[i] !== first) {
+      return null;
+    }
+  }
+  return first;
+}
+
+function checkCols(state) {
+  for (let i = 0; i < state.length; i++) {
+    const col = state.map((row) => row[i]);
+    const winner = checkRow(col);
+    if (winner) {
+      return winner;
+    }
+  }
+  return null;
+}
+
+function checkDiags(state) {
+  const diag1 = state.map((row, i) => row[i]);
+  const diag2 = state.map((row, i) => row[row.length - 1 - i]);
+  return checkRow(diag1) || checkRow(diag2);
+}
+
+function isBoardFull(state) {
+  for (let i = 0; i < state.length; i++) {
+    const row = state[i];
+    for (let j = 0; j < row.length; j++) {
+      if (row[j] === EMPTY) {
+        return false;
+      }
+    }
+  }
+  return true;
 }
 
 module.exports = {
-  sum,
+  createEmptyBoard,
+  updateGame,
+  checkWinner,
+  EMPTY,
+  P1,
+  P2,
 };
--- game.spec.js
+++ game.spec.js
@@ -1,13 +1,172 @@
-const { sum } = require("./game");
+const {
+  createEmptyBoard,
+  updateGame,
+  checkWinner,
+  EMPTY,
+  P1,
+  P2,
+} = require("./game");
 
-describe("sum", () => {
-  test("1+1", () => {
-    expect(sum(1, 2)).toBe(3);
+describe("createEmptyBoard", () => {
+  test("creates empty board", () => {
+    const board = createEmptyBoard();
+    expect(board).toEqual([
+      [EMPTY, EMPTY, EMPTY],
+      [EMPTY, EMPTY, EMPTY],
+      [EMPTY, EMPTY, EMPTY],
+    ]);
   });
-  test("2+2", () => {
-    expect(sum(2, 2)).toBe(4);
+});
+
+describe("updateGame", () => {
+  test("update success", () => {
+    const state = [
+      [P1, EMPTY, EMPTY],
+      [EMPTY, EMPTY, EMPTY],
+      [EMPTY, EMPTY, EMPTY],
+    ];
+
+    const [newState, success] = updateGame(P2, [1, 1], state);
+    expect(success).toBe(true);
+    expect(newState).toEqual([
+      [P1, EMPTY, EMPTY],
+      [EMPTY, P2, EMPTY],
+      [EMPTY, EMPTY, EMPTY],
+    ]);
   });
-  test("-100+3", () => {
-    expect(sum(-100, 3)).toBe(-97);
+
+  test("update failure", () => {
+    const state = [
+      [P1, EMPTY, EMPTY],
+      [EMPTY, EMPTY, EMPTY],
+      [EMPTY, EMPTY, EMPTY],
+    ];
+    const [newState, success] = updateGame(P2, [0, 0], state);
+    expect(success).toBe(false);
+    expect(newState).toEqual([
+      [P1, EMPTY, EMPTY],
+      [EMPTY, EMPTY, EMPTY],
+      [EMPTY, EMPTY, EMPTY],
+    ]);
+  });
+
+  test("out of bounds", () => {
+    const state = [
+      [P1, EMPTY, EMPTY],
+      [EMPTY, EMPTY, EMPTY],
+      [EMPTY, EMPTY, EMPTY],
+    ];
+    const [newState, success] = updateGame(P2, [3, 3], state);
+    expect(success).toBe(false);
+    expect(newState).toEqual([
+      [P1, EMPTY, EMPTY],
+      [EMPTY, EMPTY, EMPTY],
+      [EMPTY, EMPTY, EMPTY],
+    ]);
+  });
+});
+
+describe("checkWinner", () => {
+  test("Top row wins", () => {
+    expect(
+      checkWinner([
+        [P1, P1, P1],
+        [EMPTY, EMPTY, EMPTY],
+        [EMPTY, EMPTY, EMPTY],
+      ])
+    ).toBe(P1);
+  });
+  test("Middle row wins", () => {
+    expect(
+      checkWinner([
+        [EMPTY, EMPTY, EMPTY],
+        [P1, P1, P1],
+        [EMPTY, EMPTY, EMPTY],
+      ])
+    ).toBe(P1);
+  });
+  test("Bottom row wins", () => {
+    expect(
+      checkWinner([
+        [EMPTY, EMPTY, EMPTY],
+        [EMPTY, EMPTY, EMPTY],
+        [P1, P1, P1],
+      ])
+    ).toBe(P1);
+  });
+  test("No winner", () => {
+    expect(
+      checkWinner([
+        [P1, P2, P1],
+        [EMPTY, EMPTY, EMPTY],
+        [EMPTY, EMPTY, EMPTY],
+      ])
+    ).toBe(null);
+  });
+  test("No winner 2", () => {
+    expect(
+      checkWinner([
+        [P1, EMPTY, P1],
+        [EMPTY, P2, EMPTY],
+        [EMPTY, P2, P2],
+      ])
+    ).toBe(null);
+  });
+  test("No winner 3", () => {
+    expect(
+      checkWinner([
+        [P1, P2, P1],
+        [P1, P2, EMPTY],
+        [P2, EMPTY, EMPTY],
+      ])
+    ).toBe(null);
+  });
+
+  test("Diag 1", () => {
+    expect(
+      checkWinner([
+        [EMPTY, EMPTY, P1],
+        [EMPTY, P1, EMPTY],
+        [P1, EMPTY, EMPTY],
+      ])
+    ).toBe(P1);
+  });
+  test("Diag 2", () => {
+    expect(
+      checkWinner([
+        [P1, EMPTY, EMPTY],
+        [EMPTY, P1, EMPTY],
+        [EMPTY, EMPTY, P1],
+      ])
+    ).toBe(P1);
+  });
+  test("Diag 3", () => {
+    expect(
+      checkWinner([
+        [EMPTY, EMPTY, P2],
+        [EMPTY, P2, EMPTY],
+        [P2, EMPTY, EMPTY],
+      ])
+    ).toBe(P2);
+  });
+
+  test("col 0", () => {
+    expect(
+      checkWinner([
+        [P2, EMPTY, P2],
+        [P2, P2, EMPTY],
+        [P2, EMPTY, EMPTY],
+      ])
+    ).toBe(P2);
+  });
+
+  test("col 1", () => {
+    expect(
+      checkWinner([
+        [P1, P2, P2],
+        [EMPTY, P2, EMPTY],
+        [EMPTY, P2, EMPTY],
+      ])
+    ).toBe(P2);
   });
 });
